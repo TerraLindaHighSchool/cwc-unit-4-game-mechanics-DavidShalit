b2c37ee9bbba735908defb4318d280e1283dda5e
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-02-03 14:56:32 -0800

    4.2.5

---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyPrefab;
+    void Start()
+    {
+        Instantiate(enemyPrefab, new Vector3(0, 0, 0), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
