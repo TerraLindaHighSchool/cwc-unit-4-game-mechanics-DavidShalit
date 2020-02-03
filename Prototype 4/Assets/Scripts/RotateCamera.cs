131f18d7984d631639032a5f2b9c8ffdc76dd925
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-02-03 14:51:30 -0800

    Started 4.2

--- Prototype 4/Assets/Scripts/RotateCamera.cs
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float rotationSpeed;
     void Start()
     {
         
@@ -13,6 +14,7 @@ public class RotateCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
