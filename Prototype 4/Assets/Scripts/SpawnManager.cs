8515d97bd123ac87c1597d240670c9144e1cee3f
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-02-03 15:01:06 -0800

    Finished 4.2

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -6,11 +6,20 @@ public class SpawnManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 0), enemyPrefab.transform.rotation);
+        
+        Instantiate(enemyPrefab,GenerateSpawnPos(), enemyPrefab.transform.rotation);
     }
 
+    private Vector3 GenerateSpawnPos()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
     // Update is called once per frame
     void Update()
     {
