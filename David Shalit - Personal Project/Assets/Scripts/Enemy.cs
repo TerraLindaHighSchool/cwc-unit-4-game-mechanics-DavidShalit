84da6f1ff179e054ca1fdc010e1a31e875543724
DavidShalit <59098962+DavidShalit@users.noreply.github.com>
2020-02-04 15:33:53 -0800

    Continuing Lab 4

---
+++ David Shalit - Personal Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5.0f;
+    private Rigidbody enemyRb;
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.AddForce(Vector3.forward * -speed);
+    }
+}
